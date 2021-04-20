ecc1187f79bae99c9665e3fbebd9c5f68be86866
Kenneth Carter <kxccarter@gmail.com>
2021-04-20 15:50:22 -0500

    creates component to map api dummy data to

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useQuery } from 'react-query';
 // Components
 import { Drawer, LinearProgress, Grid, Badge } from '@material-ui/core';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Item from './Item/Item';
 // Styles
 import { Wrapper } from './App.styles';
 // Types
@@ -24,9 +25,29 @@ const getProducts = async (): Promise<CartItemType[]> => {
 const App = () => {
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     'products',
-    getProducts
+    getProducts!
   );
-  return <div className="App">Yo dawg we be coding</div>;
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Oh no something went wrong!</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
